6444701937820b4956a9cd3a70f5b060d91ffb87
Marcin Krawczyk <marcin.krawczyk@ef.com>
2020-04-14 23:49:47 +0200

    Add dummy early return for handicaped, asd ie11

--- src/index.js
+++ src/index.js
@@ -3,27 +3,26 @@ import { useInView } from "react-intersection-observer";
 import get from "lodash/get";
 // eslint-disable-next-line import/no-unresolved
 import classNames from "classnames";
-import { isIE } from "react-device-detect";
 
 /*
     Documentation:
         To use withFade HOC
         1) wrap component you like to have fade plugin, 
         2) add fade-plugin schema to component
-        3) add props.className variable to component className prop. withFade 
+        3) add props.withFadeClassName variable to component className prop. withFade 
         exposes additional classes created based on consumer input from storyblok plugin
-        4) add style props "style={props.style}"
+        4) add style props "style={props.withFadeStyle}"
 outl
         example for TextBlock component:
         1. export default withFade(TextBlock)
         2. 
             fade_plugin: {
                 type: "custom",
-                field_type: "ef-component-fade-plugin",
+                field_type: "component-fade-plugin",
                 options: [],
                 description: "Plugin for fading components into view."
             },
-        3. <div className={`ef-text-block ${props.className}`} style={props.style}>
+        3. <div className={`text-block ${props.withFadeClassName}`} style={props.withFadeStyle}>
 
         * additionaly, you can use withFade HOC without storyblok plugin, by passing 
         configuration object to it:
@@ -50,62 +49,66 @@ outl
 */
 
 export const withFade = WrappedComponent => options => {
-  if (isIE) {
-    return props => (
-      <WrappedComponent
-        {...props}
-        withFadeStyle={{ transition: "none", transform: "none", opacity: "1" }}
-      />
+  // early return for ie11 being weakest thing on earth
+  if (window !== undefined) {
+    if (window && window.navigator.userAgent.indexOf("Trident") !== -1) {
+      return props => {
+        return (
+          <WrappedComponent
+            {...props}
+            withFadeClassName="before-fade after-fade"
+          />
+        );
+      };
+    }
+  }
+
+  return props => {
+    // early return when fade not enabled
+    if (!get(props, "blok.fade_plugin.enabled") && !get(options, "enabled")) {
+      // eslint-disable-next-line react/prop-types
+      const { blok, ...restProps } = props;
+      // eslint-disable-next-line react/prop-types
+      const { fade_plugin, ...restBlok } = blok;
+
+      return <WrappedComponent blok={restBlok} {...restProps} />;
+    }
+    // eslint-disable-next-line no-unused-vars
+    const [ref, inView, entry] = useInView(
+      get(options, "config") || get(props, "blok.fade_plugin.config") || {}
     );
-  } else {
-    return props => {
-      // early return
-      if (!get(props, "blok.fade_plugin.enabled") && !get(options, "enabled")) {
-        // eslint-disable-next-line react/prop-types
-        const { blok, ...restProps } = props;
-        // eslint-disable-next-line react/prop-types
-        const { fade_plugin, ...restBlok } = blok;
 
-        return <WrappedComponent blok={restBlok} {...restProps} />;
-      }
-      // eslint-disable-next-line no-unused-vars
-      const [ref, inView, entry] = useInView(
-        get(options, "config") || get(props, "blok.fade_plugin.config") || {}
-      );
-
-      const transitionConfig = get(options, "transitionConfig") ||
-        get(props, "blok.fade_plugin.transitionConfig") || {
-          duration: 300,
-          delay: 100,
-          transitionProperty: "opacity",
-          transitionTimingFunction: "ease"
-        };
-
-      const className = classNames(
-        get(options, "classNames.before") ||
-          get(props, "blok.fade_plugin.classNames.before"),
-        entry &&
-          entry.isIntersecting &&
-          (get(options, "classNames.after") ||
-            get(props, "blok.fade_plugin.classNames.after"))
-      );
-
-      const style = {
-        "--transition-duration": `${transitionConfig.duration}ms`,
-        "--transition-delay": `${transitionConfig.delay}ms`,
-        "--transition-property": transitionConfig.transitionProperty,
-        "--transition-timing-function":
-          transitionConfig.transitionTimingFunction
+    const transitionConfig = get(options, "transitionConfig") ||
+      get(props, "blok.fade_plugin.transitionConfig") || {
+        duration: 300,
+        delay: 100,
+        transitionProperty: "opacity",
+        transitionTimingFunction: "ease"
       };
 
-      return (
-        <WrappedComponent
-          {...props}
-          withFadeClassName={className}
-          withFadeStyle={style}
-          forwardedRef={ref}
-        />
-      );
+    const className = classNames(
+      get(options, "classNames.before") ||
+        get(props, "blok.fade_plugin.classNames.before"),
+      entry &&
+        entry.isIntersecting &&
+        (get(options, "classNames.after") ||
+          get(props, "blok.fade_plugin.classNames.after"))
+    );
+
+    const style = {
+      "--transition-duration": `${transitionConfig.duration}ms`,
+      "--transition-delay": `${transitionConfig.delay}ms`,
+      "--transition-property": transitionConfig.transitionProperty,
+      "--transition-timing-function": transitionConfig.transitionTimingFunction
     };
-  }
+
+    return (
+      <WrappedComponent
+        {...props}
+        withFadeClassName={className}
+        withFadeStyle={style}
+        forwardedRef={ref}
+      />
+    );
+  };
 };
